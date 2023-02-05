9e0c10e066b27635a83aa0e203022119b84bee4e
varunvarma05 <varunvarmaracharla@gmail.com>
2023-02-05 23:26:17 +0530

    19 commit

--- src/strings/LengthExample.java
+++ src/strings/LengthExample.java
@@ -6,5 +6,5 @@ public class LengthExample {
         String s2="python";
         System.out.println("string length is: "+s1.length());//10 is the length of java point string
         System.out.println("string length is: "+s2.length());//6 is the length of python string
-    }}
+    }
 }
---
+++ src/strings/ReplaceExample.java
@@ -0,0 +1,9 @@
+package strings;
+
+public class ReplaceExample {
+    public static void main(String args[]){
+        String s1="javatpoint is a very good website";
+        String replaceString=s1.replace('a','e');//replaces all occurrences of 'a' to 'e'
+        System.out.println(replaceString);
+    }
+}
