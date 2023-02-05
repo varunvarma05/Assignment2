ffb617c71ddab96a3989067c2ad46c9e20a97d0d
varunvarma05 <varunvarmaracharla@gmail.com>
2023-02-05 23:36:42 +0530

    25 commit

---
+++ src/strings/StringLowerExample.java
@@ -0,0 +1,9 @@
+package strings;
+
+public class StringLowerExample {
+    public static void main(String args[]){
+        String s1="JAVATPOINT HELLO stRIng";
+        String s1lower=s1.toLowerCase();
+        System.out.println(s1lower);
+    }
+}
