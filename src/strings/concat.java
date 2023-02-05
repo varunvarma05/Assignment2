4169eb0f118012e825400d27f6801a1cca7425f8
varunvarma05 <varunvarmaracharla@gmail.com>
2023-02-05 23:07:32 +0530

    fifth commit

---
+++ src/strings/concat.java
@@ -0,0 +1,12 @@
+package strings;
+
+public class concat{
+    public static void main(String[] args){
+        String s1="java string";
+        s1.concat("is immutable");
+        System.out.println(s1);
+         s1=s1.concat(" is immutable so assign it explicitly");
+        System.out.println(s1);
+}
+}
+
