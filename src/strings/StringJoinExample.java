6b2c0069cf59717029ddbc78808bed3b7c4a5cdf
varunvarma05 <varunvarmaracharla@gmail.com>
2023-02-05 23:20:50 +0530

    16 commit

---
+++ src/strings/StringJoinExample.java
@@ -0,0 +1,8 @@
+package strings;
+
+public class StringJoinExample {
+    public static void main(String args[]){
+        String joinString1=String.join("-","welcome","to","javatpoint");
+        System.out.println(joinString1);
+    }
+}
