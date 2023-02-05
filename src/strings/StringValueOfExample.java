199b3e3b6d0f645b934ba223b43de6620c3306fe
varunvarma05 <varunvarmaracharla@gmail.com>
2023-02-05 23:38:31 +0530

    28 commit

---
+++ src/strings/StringValueOfExample.java
@@ -0,0 +1,9 @@
+package strings;
+
+public class StringValueOfExample {
+    public static void main(String args[]){
+        int value=30;
+        String s1=String.valueOf(value);
+        System.out.println(s1+10);//concatenating string with 10
+    }
+}
