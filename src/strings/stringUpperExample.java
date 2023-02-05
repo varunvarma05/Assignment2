40efe4fa9028551a5953bb22166c5c4761b8007c
varunvarma05 <varunvarmaracharla@gmail.com>
2023-02-05 23:37:15 +0530

    26 commit

---
+++ src/strings/stringUpperExample.java
@@ -0,0 +1,9 @@
+package strings;
+
+public class stringUpperExample {
+    public static void main(String args[]){
+        String s1="hello string";
+        String s1upper=s1.toUpperCase();
+        System.out.println(s1upper);
+    }
+}
