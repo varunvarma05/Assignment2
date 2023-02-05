bb67bb11b047753055b5adffabf6671025020f69
varunvarma05 <varunvarmaracharla@gmail.com>
2023-02-05 22:55:17 +0530

    third commit

--- StringFunctions.java
+++
@@ -1,7 +0,0 @@
-package strings;
-
-public class StringFunctions {
-    public static void main(String[] args) {
-        System.out.println("Hello");
-    }
-}
---
+++ src/strings/ChatAt.java
@@ -0,0 +1,9 @@
+package strings;
+public class ChatAt {
+    public static void main(String[] args) {
+        String name = "javaPoint";
+        char ch = name.charAt(3);
+        System.out.println(ch);
+
+    }
+}
