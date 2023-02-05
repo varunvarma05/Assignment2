0478bcbc1f6779e5386de0f7d23d23576c93475f
varunvarma05 <varunvarmaracharla@gmail.com>
2023-02-05 23:10:35 +0530

    seventh commit

---
+++ src/strings/EndsWith.java
@@ -0,0 +1,9 @@
+package strings;
+
+public class EndsWith {
+    public static void main(String[] args){
+        String s1="java by javaPoint";
+        System.out.println(s1.endsWith("t"));
+        System.out.println(s1.endsWith("point"));
+}
+}
